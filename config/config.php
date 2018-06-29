58631481bc88610499bbe224bc26180c2d205f8b
Emmanuel C <payfucallback@gmail.com>
2018-06-29 14:18:33 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -13,17 +13,11 @@ return array(
   "copyright"=>"20xx - " . date('Y') . "  © Name",
 
   // BDD
-/*
+
   "db_user" => "root",
   "db_pass" => "",
   "db_name" => "db_database",
   "db_host" => "localhost",*/
-
-  "db_user" => "refectoire.agent",
-  "db_pass" => "GZXWfU06KxYqy7qx",
-  "db_name" => "refectoire",
-  "db_host" => "172.18.10.232",
-
 
   // Contact
   "contact_destinataire"=>"contact@domaine.com",
