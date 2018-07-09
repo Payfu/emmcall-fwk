1a412173ce40e44288f0fe0a42048a87bc10d400
Emmanuel C <payfucallback@gmail.com>
2018-07-09 10:12:10 +0200

    Add files via upload

--- app/Src/TestBundle/Views/index.php
+++ app/Src/TestBundle/Views/index.php
@@ -1,1 +1,5 @@
-Ici le html
+<!-- HTML de la page index -->
+<div class="row my-3 text-center">
+  <div class="col-12"><img src="picture/favicon.png" alt=""></div>
+  <div class="col-12"><h1>Bundle</h1></div>
+</div>
