b00eb4709c2f3474e08db509ce3bb7c99a950763
Emmanuel C <payfucallback@gmail.com>
2018-10-10 09:27:44 +0200

    Ajout de la méthode DOUBLE
    _method: DOUBLE
    Permet pour une même route de recevoir les données en GET mais aussi en POST

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -20,12 +20,15 @@ class Router {
     return $this->add($path, $callable, $name, 'POST');
   }
   
+  public function double($path, $callable, $name = null){
+    return $this->add($path, $callable, $name, 'DOUBLE');
+  }
+  
   public function add($path, $callable, $name, $method){
     // L'objet route permet de travailler plus simplement
     $route = new Route($path, $callable);
     
-		//print_r($route);
-    // On crée un tableau indexé par la méthode GET ou POST
+    // On crée un tableau indexé par la méthode GET, POST ou DOUBLE
     $this->routes[$method][] = $route;
     
     // Si le $callable est une chaîne alors on en tire le nom de la route
@@ -37,26 +40,42 @@ class Router {
     if($name){
       $this->namedRoutes[$name] = $route;
     }
+    
     // On retourne une instance de $route
     return $route;
   }
   
   // Vérifie si l'url tapée en paramètre correspond à l'une des urls
   public function run(){
-    
-    // J'ai besoin de connaître la méthode retournée sinon je retourne une Exception
-    if(!isset($this->routes[$_SERVER['REQUEST_METHOD']])){
+    // Si je ne connaît pas la méthode retournée je retourne une Exception
+    if(!isset($this->routes[$_SERVER['REQUEST_METHOD']]) && !isset($this->routes['DOUBLE'])){
       throw new RouterException("<p><strong>REQUEST_METHOD n'existe pas, si vous avez choisi 'POST' la page devrait donc être appellée via _POST. </strong><br />");
-    }
+    } 
     
-    // Je connais la méthode alors je sort les routes
-    foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
-      
-      
-      // Pour chaque route je check si l'url correspond à la route tapée
-      if($route->match($this->url)){
-        // Correspondance trouvée, on appel la méthode call
-        return $route->call();
+    // Impossible de placer ces conditions dans une méthode commune, cela bug.
+    if($this->routes['GET']){
+      foreach ($this->routes['GET'] as $route){
+        // Pour chaque route je check si l'url correspond à la route tapée
+        if($route->match($this->url)){
+          // Correspondance trouvée, on appel la méthode call
+          return $route->call();
+        }
+      }
+    }
+
+    if($this->routes['POST']){
+      foreach ($this->routes['POST'] as $route){
+        if($route->match($this->url)){
+          return $route->call();
+        }
+      }
+    }
+
+    if($this->routes['DOUBLE']){
+      foreach ($this->routes['DOUBLE'] as $route){
+        if($route->match($this->url)){
+          return $route->call();
+        }
       }
     }
     
--- core/Router/Routing.php
+++ core/Router/Routing.php
@@ -14,18 +14,47 @@ class Routing extends YamlParser
   private $_array = [];
   private $_url;
 
-  public function __construct(string $fileName, $url)
+  public function __construct(string $fileName, $url = null)
   {
     parent::__construct($fileName);
     // On récupère le tableau formé à partir de YML
     $this->_array = $this->getArray();
     // On récupère l'url
     $this->_url = $url;    
+    
   }
   
   public function routeManager(){
     // On contrôle les données
     $this->checkParams(); 
+  }
+  
+  /*
+   * NOTE : Par la suite il faudra une gestion des paramètres 
+   */
+  public function redirectManager($routeName, $params = null){
+    // On récupère le path de la route du nom X
+    if(!array_key_exists($routeName, $this->_array) ){
+      die("La route n'existe pas : <strong>{$routeName}</strong>");
+    }
+    
+    $routes = $this->_array;
+    $route = $routes[$routeName];
+    $path = $route["path"];
+    $chemin  = WEBROOT."{$path}";
+    
+    // Remplace les valeurs {val} etc. par les valeur du tableau params, ex : /cdsadmin/board/{username}/{id} => /cdsadmin/board/blabla/123
+    if($params){
+      foreach($params as $k=>$v){
+        $patterns[] = '/{'.$k.'}/';
+        $replacements[] = $v;
+      }
+
+      $chemin = preg_replace($patterns, $replacements, $chemin);
+    }
+    
+    // On lance la redirection
+    header("Location: {$chemin}");    
   }
   
   /*
@@ -37,10 +66,10 @@ class Routing extends YamlParser
     $arrayRoutes = $this->_array;
     
     /**
-		 * Etape 1
-		 * Gestion du PATH
-		 * On check l'ensemble des routes et des infos indiqués
-		 */
+      * Etape 1
+      * Gestion du PATH
+      * On check l'ensemble des routes et des infos indiqués
+      */
     foreach($arrayRoutes as $k => $v)
     {
       $routeName = $k;
@@ -64,31 +93,30 @@ class Routing extends YamlParser
       } 
     }
     
-		/**
-		 * Etape 2
-		 * Gestion du PATH
-		 * On check l'ensemble des routes et des infos indiqués
-		 */
-		
-		// On instancie le router
-		$router = new Router($this->_url);
-		
-		// On boucle sur le yaml et on récupère les infos utiles pour la génération des routes.
-		foreach($arrayRoutes as $k => $v){
+    /**
+     * Etape 2
+     * Gestion du PATH
+     * On check l'ensemble des routes et des infos indiqués
+     */	
+    // On instancie le router
+    $router = new Router($this->_url);
+    
+    // On boucle sur le yaml et on récupère les infos utiles pour la génération des routes.
+    foreach($arrayRoutes as $k => $v){
 			
-			$routeName = $k; // ex: nom_de_ma_route
+      $routeName = $k; // ex: nom_de_ma_route
       $tabRoute = $v; // array
-			$params = [];
-			
-			// On remplace /test/{username}/page/{id} => /test/:username/page/:id pour la gestion des paramètres
-    	$path  = str_replace('{', ':', str_replace('}', '', $tabRoute['path']));
-			$controller = $tabRoute['defaults']["_controller"]; 
-			
-			// Si requirements est renseigné
-			if(array_key_exists('requirements', $tabRoute)){
-				// On récupère la valeur facultative _method (GET par defaut)
-				$method = !empty($tabRoute['requirements']["_method"]) ? $tabRoute['requirements']["_method"] : 'GET'; 
-				// je vérifie toute les clefs qui ne commencent pas par "_", ce sont alors des noms de paramètre
+      $params = [];
+      
+      // On remplace /test/{username}/page/{id} => /test/:username/page/:id pour la gestion des paramètres
+      $path  = str_replace('{', ':', str_replace('}', '', $tabRoute['path']));
+      $controller = $tabRoute['defaults']["_controller"]; 
+	
+      // Si requirements est renseigné
+      if(array_key_exists('requirements', $tabRoute)){        
+        // On récupère la valeur facultative _method (GET par defaut)
+        $method = !empty($tabRoute['requirements']["_method"]) ? $tabRoute['requirements']["_method"] : 'GET'; 
+        // je vérifie toute les clefs qui ne commencent pas par "_", ce sont alors des noms de paramètre
         foreach($tabRoute['requirements'] as $nomParam => $valParam){
           //$nomParam[0] = premier caractère					
           if($nomParam[0] != '_'){
@@ -96,21 +124,32 @@ class Routing extends YamlParser
             $params[$nomParam] = $valParam; 
           }
         }
-			} else { $method = 'GET';	}
-						
-			// Si get sinon post, ex : $router->get('/test', "Test:Index:maMethode");
-			$laRoute = (strtolower($method) === "get") ? $router->get($path, $controller) : $router->post($path, $controller);
-			
-			// Gestion de paramètres 
-			// Normalement la méthode with() est chaînée, mais ici je l'ajoute dynamiquement
-			if(count($params) > 0){
-				foreach ($params as $nomParam => $regex) {        
-					$laRoute->with($nomParam, $regex);
-				}
-			}
-		} // endForeach
+      } else { $method = 'GET';	}
+      
+      // Si get sinon post, ex : $router->get('/test', "Test:Index:maMethode");
+      //$laRoute = (strtolower($method) === "get") ? $router->get($path, $controller) : $router->post($path, $controller);
+	
+      // Soit GET, soit DOUBLE, soit POST
+      if(strtolower($method) === "get"){
+        $laRoute = $router->get($path, $controller);
+      } 
+      if (strtolower($method) === "double"){
+        $laRoute = $router->double($path, $controller);
+      } 
+      if (strtolower($method) === "post") {
+        $laRoute = $router->post($path, $controller);
+      }
+            
+      // Gestion de paramètres 
+      // Normalement la méthode with() est chaînée, mais ici je l'ajoute dynamiquement
+      if(count($params) > 0){
+        foreach ($params as $nomParam => $regex) {        
+          $laRoute->with($nomParam, $regex);
+        }
+      }
+    } // endForeach
 		
-		// On lance la route
+    // On lance la route
     $router->run();
   }
 	
