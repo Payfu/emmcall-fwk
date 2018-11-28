ce7b197cbbdae0fc24c7e9b37dd8169775a2b338
Emmanuel C <payfucallback@gmail.com>
2018-11-28 09:27:23 +0100

    Ajout de Slugify( )

--- core/Tools/Tools.php
+++ core/Tools/Tools.php
@@ -74,6 +74,37 @@ class Tools
         if($replaceBy){ $str = preg_replace('#(?:(['.$rep.'])\1)\1*#', $rep, $str); }
         return $str; 
     }
+    
+    /*
+     * Slugify
+     */
+    public function slugify($str)
+    {
+      
+      // replace non letter or digits by -
+      $text = preg_replace('~[^\pL\d]+~u', '-', $str);
+
+      // transliterate
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+      // remove unwanted characters
+      $text = preg_replace('~[^-\w]+~', '', $text);
+
+      // trim
+      $text = trim($text, '-');
+
+      // remove duplicate -
+      $text = preg_replace('~-+~', '-', $text);
+
+      // lowercase
+      $text = strtolower($text);
+
+      if (empty($text)) {
+        return 'n-a';
+      }
+
+      return $text;
+    }
 
 
     /*
