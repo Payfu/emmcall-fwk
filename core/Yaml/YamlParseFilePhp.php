1db32d7fc0ea00e76ea0bc659f8ac82068c8af27
Emmanuel C <payfucallback@gmail.com>
2020-12-03 15:52:15 +0100

    Update YamlParseFilePhp.php
    Modification de cette classe de pour convertir les chaînes true, false (et null) en boolean

--- core/Yaml/YamlParseFilePhp.php
+++ core/Yaml/YamlParseFilePhp.php
@@ -14,14 +14,12 @@ class YamlParseFilePhp
     
     $arrFinal = [];
     foreach (new \SplFileObject($fileName) as $line) {
-      
       // Si le premier caractère de la ligne est un '#' alors c'est un commentaire et on ne tient pas compte de cette ligne.
       // Mais il faut également que la ligne contienne au moins 2 caractères de type alphanumérique
       if(substr(trim($line), 0,1) !== '#' && preg_match("/[a-z0-9_,]{2}/", $line)){
         
         // On compte le nombre de fois où les 4 espaces (obligation YAML) sont présents
-        $nv = substr_count ($line, '    ');
-        
+        $nv = $this->checkLine($line);
         // On enlève les espaces avant et après
         $cLine = trim($line);        
                
@@ -34,7 +32,6 @@ class YamlParseFilePhp
         
         // Niveau 1 : path, defaults, requirements et - nomFichierRoute
         if($nv == 1){  
-          
           // Il y a 3 cas possibles
           // 1/ = aucun ':' ce sont les noms de fichier => "- nomFichierRoute"
           // 2/ = Il y a les ':', on explode val[0] = string et val[1] = ""
@@ -50,7 +47,7 @@ class YamlParseFilePhp
             } 
             // cas 3
             else {
-              $arrFinal[$keyNv0][$v[0]] = trim($v[1]);
+              $arrFinal[$keyNv0][$v[0]] = $this->convertBool(trim($v[1]));
             }
           }
           // cas 1
@@ -63,12 +60,12 @@ class YamlParseFilePhp
         // Niveau 2 : _controller, _methode ou clef libre
         if($nv == 2){
           $v = explode(':', $cLine, 2);
-          
           if(isset($v[1])){
             // On supprime les espaces avant et après mais aussi les éventuelles '"' (guillement double avant et après)
-            $arrFinal[$keyNv0][$keyNv1][$v[0]] = trim(trim($v[1]), '"');
+            $arrFinal[$keyNv0][$keyNv1][$v[0]] = $this->convertBool( trim(trim($v[1]), '"') );
           } else {
-            $arrFinal[$keyNv0][$keyNv1] = trim(trim($v[0]), '"');
+            
+            $arrFinal[$keyNv0][$keyNv1] = $this->convertBool( trim(trim($v[0]), '"') );
           }
           $keyNv2 = isset($v[0]) ? $v[0] : null;
         }
@@ -76,17 +73,43 @@ class YamlParseFilePhp
         // Niveau 3 : informations de connexion à la base de donnée
         if($nv == 3){
           $v = explode(':', $cLine, 2);
-          // si les données sont présenté comme ceci : "db_type: sqlsrv/mysql" ou juste ceci "sqlsrv/mysql"
+          // si les données sont présentées comme ceci : "db_type: sqlsrv/mysql" ou juste ceci "sqlsrv/mysql"
           if(isset($v[1])){
             // On supprime les espaces avant et après mais aussi les éventuelles '"' (guillement double avant et après)
-            $arrFinal[$keyNv0][$keyNv2][$v[0]] = trim(trim($v[1]), '"');
+            $arrFinal[$keyNv0][$keyNv2][$v[0]] = $this->convertBool( trim(trim($v[1]), '"') );
           } else {
-            $arrFinal[$keyNv0][$keyNv2] = trim(trim($v[0]), '"');
+            $arrFinal[$keyNv0][$keyNv2] = $this->convertBool( trim(trim($v[0]), '"') );
           }
         }
       }
     }
-    
     return $arrFinal;    
+  }
+  
+  /*
+   * Si la ligne contient une séquence de 4 espaces => '    '
+   * Retourne int
+   */
+  private function checkLine($line):int{
+    // On identifie les chaînes qui commencent par un espace.
+    // On tiens compte du début de la chaîne car il n'est pas impossible qu'il y ai des espaces en fin de chaîne.
+    $matches = [];
+    if(preg_match('/(^\s{4,})/i', $line, $matches)){
+      // On compte le nombre de fois où les 4 espaces (obligation YAML) sont présents
+      return substr_count ($matches[0], '    ');
+    }
+    return 0;
+  }
+  
+  /*
+   * Si les chaînes : TRUE, FALSE et NULL sont présentes alors on les converti dans leurs valeurs respectives
+   */
+  private function convertBool($str){
+    // Si boolean, voir aussi https://www.w3schools.com/php/filter_validate_boolean.asp
+    if(in_array(strtolower($str), ['true', 'false'])){  return mb_strtolower($str) === 'true'? true: false; }
+    // Si NULL
+    if(mb_strtolower($str) === 'null'){ return null; }
+    // On ne fait rien.
+    return $str;
   }
 }
