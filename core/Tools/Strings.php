0100fbcbaa50a4f8025af53e516a077675d7d404
Emmanuel C <payfucallback@gmail.com>
2021-03-23 11:52:14 +0100

    Update Strings.php

--- core/Tools/Strings.php
+++ core/Tools/Strings.php
@@ -85,14 +85,61 @@ class Strings
     return $text;
   }
   
-  public static function autoCopyright($year = 'auto')
-  {  
-  
+  public static function autoCopyright($year = 'auto'){  
     if(intval($year) == 'auto'){ $year = date('Y'); }
     if(intval($year) == date('Y')){ $copyright = intval($year); }
     if(intval($year) < date('Y')){ $copyright = intval($year) . ' - ' . date('Y'); }
     if(intval($year) > date('Y')){ $copyright = date('Y'); }
     
     return $copyright;
+  }
+  
+  /*
+   * On crypte la valeur en la glissant entre deux chaînes aléatoires de 5 caractères
+   * On encode en base64 mais on retire le "=" à la fin
+   */
+  public static function encodeStr($str = false, $numGenerate = 5) : string {  
+    if(!isset($str) && !$str){ exit("Veuillez indiquer une chaîne à encoder"); }
+ 
+    // On encode la chaine base 64
+    //$base64 = base64_encode( $this->generate(5).$str.$this->generate(5) );
+    $base64 = base64_encode( Password::generate($numGenerate).$str.Password::generate($numGenerate) );
+    // On compte le nom de signe '=' à la fin
+    $n = substr_count($base64,"=");
+    // S'il y a au moins un =
+    if($n > 0){
+      $base64 = substr( $base64, 0, '-'.$n);
+    }
+    // On place le nombre de '=' en fin de chaine
+    $str = $base64.$n;
+    
+    return str_replace("/", "-", strrev($str) );
+  }
+  
+  /*
+   * On décode la chaîne pour récupérer la partie qui nous intéresse
+   * On remet le '=' à la fin puis on décode
+   * Enfin on retire les 5 premiers et 5 derniers caractères de la chaine
+   */
+  public static function decodeStr($str, $numGenerated = 5) : string {
+    
+    // on retourne la variable sans modification si celle-ci à une taille inférieure à 10 (voir encodeStr( )) 
+    if(strlen($str) <= 10){ return $str; }
+    
+    $a = str_replace("-", "/", $str);
+    $s = strrev($a);
+    // on récupère le dernier chiffre
+    $n = substr($s, -1);
+    // on récupère la chaine SANS le dernier chiffre
+    $s = substr($s, 0, -1);
+    // On répète le '=' autant de fois que nécessaire
+    $nn = str_repeat("=", $n);
+    // On ajoute les '=', s'ils extiste
+    $str = $s.$nn;
+    // On décode
+    $cDecode = base64_decode($str);
+    // On récupère la chaîne
+    //return substr(substr($cDecode, 0, -5), 5) ;
+    return substr(substr($cDecode, 0, -$numGenerated), $numGenerated) ;
   }
 }
