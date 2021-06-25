60a8651e30083df001a010850d664cfdba03a86e
Emmanuel C <payfucallback@gmail.com>
2021-06-25 10:05:09 +0200

    Update TypeDataBase.php

--- core/DataBase/TypeDataBase.php
+++ core/DataBase/TypeDataBase.php
@@ -63,6 +63,7 @@ class TypeDataBase extends DataBase
       if($this->_db_type === "MYSQL"){
         $options = array( PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
         $pdo = new PDO('mysql:dbname='.$this->_db_name.';host='.$this->_db_host, $this->_db_user, $this->_db_pass, $options);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       }
 
